67cde5b370a681f4f6d56b35254c7d14b7235885
Marcus Berggren <berggren.me@outlook.com>
2024-06-24 21:08:14 +0200

    Remove files and update names

--- vessel_info.py
+++ vessel_info.py
@@ -1,40 +1,77 @@
-from typing import Optional, Dict, Union
+from collections import defaultdict
+import html
 
-from src.parse_data import extract_vessel_info
-from src.selenium_chrome import get_information_from_chrome_search
-from src.vessel_database import create_vessel_in_database, count_vessels_in_db
+from bs4 import BeautifulSoup
+
+from src.output import init_program, print_dict_info, print_final_info
+from src.vessel_database import count_vessels_in_db, create_vessel_in_database
+from src.webdriver import setup_driver
+from src.utility import (create_dict_from_list, string_found, date_found, find_regex_matches, get_max_key_value,
+                         find_regex_matches_for_imo,
+                         find_regex_matches_for_mmsi, find_regex_matches_for_callsign)
 
 
 def main() -> None:
-    print(f"-----------------Create vessel in database------------------\n"
-          f"Type the name of the vessel you wish to add to the database.\n"
-          f"Upper or lower case letters does not matter.\n"
-          f"There are currently {count_vessels_in_db()} vessels in database.")
-    vessel_name = input("Type the name here: ")
-    print("Now we wait a few seconds.")
+    """
 
-    html_content = get_information_from_chrome_search(vessel_name)
-    if not html_content:
-        print(f"Vessel {vessel_name.upper()} not found. Try search for a new vessel.")
+    :return: None
+    """
+
+    vessel = init_program(count_vessels_in_db()).strip().lower()
+    if not vessel:
         return
-    vessel_info = extract_vessel_info(html_content)
 
-    name = vessel_info.get('name')
-    imo = vessel_info.get('imo')
-    callsign = vessel_info.get('callsign')
-    mmsi = vessel_info.get('mmsi')
+    url = f"https://www.google.com/search?q=container+vessel+{vessel}+imo+call+sign+mmsi+marinetraffic"
 
-    print(
-        f"\n"
-        f"NAME: {name.upper()}\n"
-        f"IMO: {imo}\n"
-        f"CALLSIGN: {callsign}\n"
-        f"MMSI: {mmsi}\n"
-    )
+    # Webdriver to launch headless Edge browser
+    with setup_driver() as driver:
+        driver.get(url)
+        data = driver.page_source
 
-    data = (imo, name, callsign, mmsi)
-    create_vessel_in_database(data)
-    print("Vessel created or updated in database.")
+    soup = BeautifulSoup(data, 'html.parser')
+    # content = soup.find(id="res")
+    content = soup.find(id="rso")
+
+    # Find the div with id "rso"
+    rso_div = soup.find('div', {'id': 'rso'})
+
+    # Define the possible values of data-snf
+    data_dnf_values = ['nke7rc', 'x5WNvb', 'oyZ5Hb']
+
+    text = ""
+    # Extract and print the span information from each target div
+    for value in data_dnf_values:
+        target_divs = rso_div.find_all('div', {'data-snf': value})
+        for div in target_divs:
+            #spans = div.find_all('span')
+            span = div.find('span')
+            print(span.text)
+            """for span in spans:
+                text += span.text
+                print(span.text)"""
+            text += span.text
+
+    soup_str = str(soup) + str(content) + text
+    imo_list = find_regex_matches_for_imo(soup_str)
+    mmsi_list = find_regex_matches_for_mmsi(soup_str)
+    callsign_list = find_regex_matches_for_callsign(soup_str)
+
+    imo_dict = create_dict_from_list(imo_list)
+    mmsi_dict = create_dict_from_list(mmsi_list)
+    callsign_dict = create_dict_from_list(callsign_list)
+
+    print_dict_info(vessel, imo_dict, mmsi_dict, callsign_dict)
+
+    vessel = vessel.upper()
+    imo: int = get_max_key_value(imo_dict)
+    mmsi: int = get_max_key_value(mmsi_dict)
+    callsign: str = get_max_key_value(callsign_dict)
+
+    status = create_vessel_in_database((imo, vessel, callsign, mmsi))
+    """if status:
+        print_final_info(vessel, imo, mmsi, callsign)"""
+
+    print_final_info(vessel, imo, mmsi, callsign)
 
 
 if __name__ == '__main__':
